4b89f9c49742614f09fce916a78feb3ad627c9b0
noless3011 <botay301104@gmail.com>
2024-08-11 20:46:56 +0700

    Fix audio

--- music.js
+++ music.js
@@ -6,10 +6,11 @@ export function GetAudioStream(link){
     const stream = ytdl(link, {
         filter: "audioonly",
         fmt: "opus",
-        highWaterMark: 1 << 62,
-        liveBuffer: 1 << 62,
+        highWaterMark: 1 << 20,
+        liveBuffer: 1 << 64,
         dlChunkSize: 0, //disabling chunking is recommended in discord bot
-        bitrate: 128,
-      });
+        quality: 'highestaudio',
+        bitrate: 600,
+      },{highWaterMark: 1});
     return stream;
 }
